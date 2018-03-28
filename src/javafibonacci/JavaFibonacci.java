7291d38e59345942a1c89a4b988163ba3fefecf2
baihaqi0031 <37771959+baihaqi0031@users.noreply.github.com>
2018-03-28 12:12:20 +0700

    Update JavaFibonacci.java

--- src/javafibonacci/JavaFibonacci.java
+++ src/javafibonacci/JavaFibonacci.java
@@ -9,37 +9,63 @@ package javamodifiedsumoffibonacci;
  *
  * @author Praktikan
  */
-public class JavaModifiedSumOfFibonacci {
 
-    /**
-     * @param args the command line arguments
-     */
-static int SumFibo(int n)
-    {
-        if (n <= 0)
-           return 0;
-      
-        int fib[]=new int[n+1];
-        fib[0] = 0; fib[1] = 1;
-      
-        // Initialize result
-        int sum = fib[0] + fib[1];
-      
-        // Add remaining terms
-        for (int i=2; i<=n; i++)
-        {
-            fib[i] = fib[i-1]+fib[i-2];
-            sum += fib[i];
-        }
-      
-        return sum;
-    }
-      
-    // Driver program to test above function
-    public static void main(String args[])
-    {
-        int n = 4;
-        System.out.println("Input : "+n);
-        System.out.println("Output : "+ SumFibo(n));
-    }
+import javax.swing.JOptionPane;
+
+public class ReverseOfFibonacci {
+
+public static void main (String [] args)
+{
+int i,j,temp;
+int input = Integer.parseInt(JOptionPane.showInputDialog("masukkan integer positif : "));
+int SIZE = (input*2) +1;
+int [] array= new int [SIZE];
+int sum=2; 
+int median;
+
+
+array [0] = 0;
+array [1] = 1;
+
+
+for (i=2; i<array.length; i++) {
+array [i] = array [i-1] + array [i-2];
+}
+
+
+if (SIZE % 2 == 0) {
+median = array [(SIZE/2)-1] + array [SIZE/2];
+}
+else {
+median = array [SIZE/2];
+}
+
+
+for (i=2; i<array.length; i++) {
+sum = sum + array [i] ;
+}
+
+int mean = sum/SIZE;
+
+
+for (i=0; i<array.length-1; i++) {
+for (j=0; j<array.length-1; j++) {
+if (array [j]< array [j+1] ) {
+temp = array [j];
+array [j] = array [j+1];
+array [j+1] = temp;
+}
+}
+}
+
+System.out.println("deret : ");
+for (i=0; i<array.length; i++) {
+System.out.print (array [i]+ " ");
+}
+System.out.println ("");
+
+System.out.println ("mean :" + mean);
+System.out.println ("median:" + median);
+
+}
 }
